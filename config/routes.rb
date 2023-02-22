fa102bc59219a2482244a4ec75cee254c0a0870d
Afafe Slimani <slimani.afaf.iaa@gmail.com>
2023-02-22 18:09:00 +0100

    not finished

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants/:id', to: 'restaurants#show'
+  # get 'reviews', to: 'reviews#index'
+  # get 'reviews/new', to: 'reviews#new'
+  # post 'reviews', to: 'reviews#create'
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 end
